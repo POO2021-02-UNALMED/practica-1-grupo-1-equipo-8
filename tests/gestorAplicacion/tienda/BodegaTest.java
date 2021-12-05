8f08fa4ca9f35fc2a580b54e94f2d5f6c89168f2
Erik Gonzalez <erikhuboy@gmail.com>
2021-12-05 17:30:04 -0500

    pruebas bodega

---
+++ tests/gestorAplicacion/tienda/BodegaTest.java
@@ -0,0 +1,22 @@
+package gestorAplicacion.tienda;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+class BodegaTest {
+
+	@Test
+	void agregarYSacarComponente() {
+		Componente componente = new Componente();
+		Bodega.agregarComponente(componente);
+		assertEquals(componente, Bodega.sacarComponente(componente));
+	}
+	
+	@Test
+	void sacaComponenteQueNoExiste() {
+		Componente componente = new Componente();
+		assertNull(Bodega.sacarComponente(componente));
+	}
+}
