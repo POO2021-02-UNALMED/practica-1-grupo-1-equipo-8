c959de28b2c1bb926e24050fed8053db7f71dbcf
EsGarciaC <esgarciac@unal.edu.co>
2021-12-06 20:58:52 -0500

    Definido Empleado y definiendo Dependiente

--- src/gestorAplicacion/personal/Dependiente.java
+++ src/gestorAplicacion/personal/Dependiente.java
@@ -1,5 +1,68 @@
 package gestorAplicacion.personal;
 
+import java.util.ArrayList;
+import java.util.List;
+import gestorAplicacion.tienda.*;
+/**
+ * 
+ * @author Esteban García
+ *
+ */
 public class Dependiente extends Empleado {
-
+	
+	private CajaRegistradora cajaRegistradora;
+	private List<Servicio> servicios;
+	
+	public Dependiente(String nombre, int cedula, CajaRegistradora caja) {
+		super(nombre, cedula);
+		this.cajaRegistradora = caja;
+		this.servicios = new ArrayList<Servicio>();
+	}
+	
+	public void atenderCliente(Cliente cliente) {
+		
+	}
+	
+	public void registrarPago() {
+		
+	}
+	
+	/**
+	 * 
+	 * @param servicio
+	 * @summary El método quitarServicio recibe como parámetro un servicio y lo remueve de la lista de servicios del técnico en cuestión.
+	 * 
+	 */
+	public void quitarServicio(Servicio servicio) {
+		servicios.remove(servicio);
+		
+	}
+	
+	/**
+	 * 
+	 * @param tecnico
+	 * @param producto
+	 * @summary generar servicio crea un servicio para revisar un producto que se le asigna a la lista de servicios 
+	 * dependiente que lo creó y al técnico que va a realizarlo.
+	 * 
+	 */
+	public void generarServicio(Tecnico tecnico, Producto producto) {
+		Servicio servicio = new Servicio(); //////////Recibe qué parametros?/////////
+		servicios.add(servicio);
+		tecnico.asignarServicio(servicio);
+		
+		
+	}
+	
+	public void finalizarServicio(Servicio servicio) {
+		
+	}
+	
+	private void notificarCliente(Cliente cliente) {
+		
+	}
+	
+	private void entregarProducto(Producto producto) {
+		
+	}
 }
--- src/gestorAplicacion/personal/Empleado.java
+++ src/gestorAplicacion/personal/Empleado.java
@@ -1,5 +1,54 @@
 package gestorAplicacion.personal;
 
+import java.util.ArrayList;
+import java.util.List;
+import gestorAplicacion.tienda.*;
+/**
+ * 
+ * @author Esteban García
+ * 
+ *
+ */
 public abstract class Empleado {
 
+	private String nombre;
+	private int cedula;
+	private List<Servicio> servicios;
+	
+	public Empleado(String nombre, int cedula) {
+		this.nombre = nombre;
+		this.cedula = cedula;
+	}
+	
+	public Empleado() {
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+
+	public List<Servicio> getServicios(){
+		return servicios;
+	}
+
+	public void setServicios(List<Servicio> servicios) {
+		this.servicios = servicios;
+	}
+	
+	public abstract void quitarServicio(Servicio servicio);
+	
+	
 }
