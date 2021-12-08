a430f8db4701643d827e1ae3b7e4f52537c9ab23
Miranda46 <80054292+Miranda46@users.noreply.github.com>
2021-12-08 00:13:27 -0500

    push inicial Servicio y Componente
    push inicial de las dos clases.

--- src/gestorAplicacion/tienda/Componente.java
+++ src/gestorAplicacion/tienda/Componente.java
@@ -1,5 +1,26 @@
 package gestorAplicacion.tienda;
-
+/**
+ * 
+ * @author Felipe Miranda
+ * @summary Componente son las posibles partes que requiere un producto para ser reparado. Se almacenan en bodega. 
+ * tienen averiado (si el producto está bueno o no), precio (de acá se calcularán las ganancias), y nombre. 
+ */
 public class Componente {
-
+	private String nombre;
+	private boolean averiado;
+	private double precio;
+	public String getNombre() {
+		return nombre;
+	}
+	public boolean isAveriado() {
+		return averiado;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	
 }
--- src/gestorAplicacion/tienda/Servicio.java
+++ src/gestorAplicacion/tienda/Servicio.java
@@ -1,5 +1,63 @@
 package gestorAplicacion.tienda;
-
+import java.util.Date;
+import gestorAplicacion.personal.Dependiente;
+import gestorAplicacion.personal.Tecnico;
+/**
+ * 
+ * @author Felipe Miranda
+ * {@summary} Servicio deberá contener toda la información que relaciona a un cliente y su producto a reparar 
+ * con los empleados (el dependiente que le atendió y el técnico encargado de la reparación) del sistema. 
+ *
+ */
 public class Servicio {
+	Tecnico tecnico;
+	boolean pagado;
+	Producto producto;
+	Date fecha;
+	Cliente cliente;
+	Dependiente dependiente;
+	double costo;
+	String diagnostico;
+	
+	public Servicio() {}
 
+	public Servicio(Tecnico tecnico, boolean pagado, Producto producto, Cliente cliente,
+			Dependiente dependiente, double costo, String diagnostico) {
+		super();
+		this.tecnico = tecnico;
+		this.pagado = pagado;
+		this.producto = producto;
+		Date fecha = new Date();
+		this.cliente = cliente;
+		this.dependiente = dependiente;
+		this.costo = costo;
+		this.diagnostico = diagnostico;
+	}
+
+
+	public void pagar(){}
+
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+
+	public Dependiente getDependiente() {
+		return dependiente;
+	}
+	
+	public void anadirCosto(double costo) {
+		this.costo+=costo;
+	}
+	
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}	
+	
 }
+
