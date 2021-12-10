51a02f14319709d91791a4e48669f1e2ad15d015
Erik Gonzalez <erikhuboy@gmail.com>
2021-12-10 14:00:37 -0500

    funcionalidades test

---
+++ tests/gestorAplicacion/tienda/Funcionalides.java
@@ -0,0 +1,13 @@
+package gestorAplicacion.tienda;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Funcionalides {
+
+	@Test
+	void diagnosticar() {
+		
+	}
+}
