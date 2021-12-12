78237dcedda25a58641434df9605620a8db2e8a4
Miranda46 <80054292+Miranda46@users.noreply.github.com>
2021-12-12 15:07:57 -0500

    Serializar config
    empezando a agregar capa persistencia

--- src/baseDatos/Serializador.java
+++ src/baseDatos/Serializador.java
@@ -8,18 +8,17 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 
-import gestorAplicacion.tienda.producto;
+import gestorAplicacion.tienda.Producto;
 
 public class Serializador {
 
-	public class Serializador {
 		private static File rutaTemp = new File("sr\\baseDatos\\Temp");
 
 		public static void serializar(Producto producto) {
 			FileOutputStream fos;
 			ObjectOutputStream oos;
 			File[] docs = rutaTemp.listFiles();
-			Printwriter pw;
+			PrintWriter pw;
 
 			for (File file : docs) {
 				try {
@@ -30,6 +29,23 @@ public class Serializador {
 				}
 
 			}
+			for (File file: docs) {
+				if (file.getAbsolutePath().contains("asignatruas")) {
+					try {
+						fos = new FileOutputStream(file);
+						oos = new ObjectOutputStream(fos);
+						oos.writeObject(producto);
+						
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+						
+					
+					}catch (IOException e) {
+						e.printStackTrace();
+					
+					}
+				}
+			}
 		}
 	}
-}
+
