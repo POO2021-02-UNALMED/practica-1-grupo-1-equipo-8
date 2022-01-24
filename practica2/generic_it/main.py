c21c37d66f277af200f02b1b5ccf7e9f96f53b0e
eporrasm <vals4oo@gmail.com>
2022-01-24 17:42:06 -0500

    Formulario

--- practica2/generic_it/main.py
+++ practica2/generic_it/main.py
@@ -1,43 +1,64 @@
+# @author: Emilio Porras
+# @author: Esteban Garcia
+# @summary programa principal de la aplicacion
 from tkinter import *
 
 class FieldFrame(Frame):
     def __init__(self, master, tituloCriterios, criterios, tituloValores, valores, habilitado):
-        self.tituloCriterios = tituloCriterios
-        self.criterios = criterios
-        self.tituloValores = tituloValores
-        self.valores = valores
-        self.habilitado = habilitado
-        self.entries = []
+        self._tituloCriterios = tituloCriterios
+        self._criterios = criterios
+        self._tituloValores = tituloValores
+        self._valores = valores
+        self._habilitado = habilitado
+        self._entries = list()
         super().__init__(master)
+        self.actualizacion()
 
-        Label(self, text=self.tituloCriterios).grid(padx = 80, column = 0, row = 0)
-        Label(self, text=self.tituloValores).grid(padx = 80, column = 1, row = 0)
-        for i in range(1, len(criterios)+1):
-            Label(self, text=criterios[i-1]).grid(padx = 80,pady=2, column=0, row=i)
-            entrada = Entry(self, width = 40)
+
+    def actualizacion(self):
+        Label(self, text=self._tituloCriterios).grid(padx = 80, column = 0, row = 0)
+        Label(self, text=self._tituloValores).grid(padx = 80, column = 1, row = 0)
+        for i in range(1, len(self._valores)+1):
+            Label(self, text=self._criterios[i-1]).grid(padx = 80, pady=2, column=0, row=i)
+            if self._criterios[i-1] in self._habilitado:
+                texto = StringVar(value=self._valores[i-1])
+                entrada = Entry(self, width = 40, textvariable=texto, state=DISABLED, justify="center")
+            else:
+                texto = StringVar(value=self._valores[i-1])
+                entrada = Entry(self, width = 40, textvariable=texto, justify="center")
             entrada.grid(pady =2, column=1, row=i)
-            self.entries.append(entrada)
+            self._entries.append(entrada)
 
-        
-        aceptar = Button(self, text="Aceptar",command=self.aceptarCheck).grid(pady = 50, column = 0, row = len(self.criterios)+1)
-        borrar = Button(self, text="Borrar", command=self.borrarEntry).grid(pady = 50, column = 1, row = len(self.criterios)+1)
         
     #Funcion auxiliar del boton aceptar
     def aceptarCheck(self):
-        for entrada in self.entries:
-            print(entrada.get())
+        #EXCEPTION SI FALTAN CAMPOS POR LLENAR Y DECIR CUALES SON
+        for i in range(len(self._entries)):
+            self._valores[i] = self._entries[i].get()
+        print(self._valores)
+        print(self.getValue(self._criterios[1]))
 
     
     #Funcion auxiliar del boton borrar
     def borrarEntry(self):
-        pass
+        for entrada in self._entries:
+            entrada.delete(0, "end")
         
-        
-    
-
     def getValue(self, criterio):
-        pass   
-     
+        return dict(zip(self._criterios, self._valores))[criterio]
+    
+    def getCriterios(self): 
+        return self._criterios
+        
+    def getValores(self):
+        return self._valores
+
+    def setValores(self, valores):
+        self._valores = valores
+    
+    def setEntries(self, entries):
+        self._entries = entries
+        
         
         
             
@@ -46,7 +67,7 @@ class FieldFrame(Frame):
 
 if __name__ == "__main__":
     print('practica 2')
-
+    #Frame de creacion de cliente -------------
     window = Tk()
     window.title("Generic IT")
     window.resizable(True,True)
@@ -58,10 +79,30 @@ if __name__ == "__main__":
     descripcion = Label(procesoConsulta, text="Diligenciar la siguiente información para el correcto ingreso del cliente al sistema: ", bd= 10)
     descripcion.pack()
 
-    crearCliente = FieldFrame(procesoConsulta, "Datos cliente",["ID","Nombre", "Cedula", "Direccion", "Cartera"], "Valor", [0, "Hernesto", 1029384, "cra 34h 89-25", 500000], ["ID"])
+    #VALOR DE ID = len(Cliente.clientes)
+    crearCliente = FieldFrame(procesoConsulta, "Datos cliente",["ID","Nombre", "Cedula", "Cartera"], "Valor", [0, None, None, None], ["ID"])
     crearCliente.pack()
-
+    
     procesoConsulta.pack()
+
+    def creacionCliente():
+        crearCliente.aceptarCheck()
+        #Cliente(crearCliente.getValue("Nombre"), crearCliente.getValue("Cedula"), --Lista de productos elegidos al azar--, Dependiente.getDependientes[0], float(crearCliente.getValue("Cartera")))
+        valores = crearCliente.getValores()
+        #Actualizar id del cliente en el FieldFrame
+        crearCliente.setValores([int(valores[0]) + 1] + [valores[i] for i in range(1, len(valores))])
+        #Resetear entries del FieldFrame
+        crearCliente.setEntries(list())
+        #Refrescar el FieldFrame
+        crearCliente.actualizacion()
+        print(crearCliente.getValores())
+        print(crearCliente._entries)
+
+    
+    aceptar = Button(crearCliente, text="Aceptar",command=creacionCliente).grid(pady = 50, column = 0, row = len(crearCliente.getCriterios())+1)
+    borrar = Button(crearCliente, text="Borrar", command=crearCliente.borrarEntry).grid(pady = 50, column = 1, row = len(crearCliente.getCriterios())+1)
+    #------------------------------------------
+
     window.mainloop()
 
 
