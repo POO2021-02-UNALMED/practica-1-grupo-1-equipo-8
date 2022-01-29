6e5c501a2c095e6c3f1a0e64f6de7ed4c815744c
Erik Gonzalez <erikhuboy@gmail.com>
2022-01-29 15:56:39 -0500

    actualiza cuando presiona texto

--- practica2/generic_it/ui_main/hoja_vida.py
+++ practica2/generic_it/ui_main/hoja_vida.py
@@ -3,15 +3,15 @@ from tkinter import Label, Entry, Button, Text, PhotoImage, Frame, INSERT
 class HojaVida(Frame):
     def __init__(self, window):
         super().__init__(window)
-        self.text = Text(self)
+        self.text = None
         self.next_hv = 0
-
         self.photo = None
+
         self.cargar_hv(0)
         self.cargar_hv_imagen(0, 0)
 
     def proximo(self, _):
-        if self.next_hv < 4:
+        if self.next_hv < 3:
             self.next_hv = self.next_hv + 1
         else:
             self.next_hv = 0
@@ -23,10 +23,14 @@ class HojaVida(Frame):
         path = './assets/hv_{0}_{1}.png'.format(hv_num, numero)
         self.photo = PhotoImage(file=path)
         l = Label(self, image=self.photo)
-        l.pack()
+        l.grid(row=1, column=0)
 
     def cargar_hv(self, numero):
+        self.text = Entry(self, width=10)
+        self.text.grid(row=0, column=0)
+        self.text.bind('<Button-1>', self.proximo)
+
         path = './assets/hv{0}.txt'.format(numero)
+
         with open(path, "r+") as hv_text:
             self.text.insert(INSERT, hv_text.read())
-        self.text.pack()
