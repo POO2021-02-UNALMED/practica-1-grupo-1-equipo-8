3e994b4802618340d920a4ef62ec4ebacf7bcd34
Erik Gonzalez <erikhuboy@gmail.com>
2022-01-29 16:27:34 -0500

    hoja de vida

--- practica2/generic_it/ui_main/hoja_vida.py
+++ practica2/generic_it/ui_main/hoja_vida.py
@@ -3,10 +3,14 @@ from tkinter import Label, Entry, Button, Text, PhotoImage, Frame, INSERT
 class HojaVida(Frame):
     def __init__(self, window):
         super().__init__(window)
+        self.p5 = Frame(self)
+        self.p5.grid()
+        entry = Entry(self.p5)
+        entry.grid(row=0, column=0)
+        entry.insert(INSERT, 'Hoja de vida')
         self.text = None
         self.next_hv = 0
         self.photo = None
-
         self.cargar_hv(0)
         self.cargar_hv_imagen(0, 0)
 
@@ -23,11 +27,11 @@ class HojaVida(Frame):
         path = './assets/hv_{0}_{1}.png'.format(hv_num, numero)
         self.photo = PhotoImage(file=path)
         l = Label(self, image=self.photo)
-        l.grid(row=1, column=0)
+        l.grid(row=2, column=0)
 
     def cargar_hv(self, numero):
-        self.text = Entry(self, width=10)
-        self.text.grid(row=0, column=0)
+        self.text = Text(self.p5, height=10)
+        self.text.grid(row=1, column=0)
         self.text.bind('<Button-1>', self.proximo)
 
         path = './assets/hv{0}.txt'.format(numero)
