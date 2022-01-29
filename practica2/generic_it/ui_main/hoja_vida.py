865e9bcd7876033efd42041f714df94ed168cbaf
Erik Gonzalez <erikhuboy@gmail.com>
2022-01-29 15:17:15 -0500

    hv cargar

--- practica2/generic_it/ui_main/hoja_vida.py
+++ practica2/generic_it/ui_main/hoja_vida.py
@@ -3,10 +3,19 @@ from tkinter import Label, Entry, Button, Text, PhotoImage, Frame, INSERT
 class HojaVida(Frame):
     def __init__(self, window):
         super().__init__(window)
-        text = Text(self)
+        self.text = Text(self)
+        self.photo = None
+        self.cargar_hv(1)
+        self.cargar_hv_imagen(1)
+
+
+    def cargar_hv_imagen(self, numero):
         self.photo = PhotoImage(file="./assets/goku.png")
-        text.insert(INSERT, "Prueba")
-        text.pack()
         l = Label(self, image=self.photo)
         l.pack()
 
+    def cargar_hv(self, numero):
+        path = './assets/hv{0}.txt'.format(numero)
+        with open(path, "r+") as hv_text:
+            self.text.insert(INSERT, hv_text.read())
+        self.text.pack()
