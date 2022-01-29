1988d8612c7e8821616275d9ae16da80655f382a
Erik Gonzalez <erikhuboy@gmail.com>
2022-01-28 20:25:53 -0500

    muevo field frame para otro archivo

--- practica2/generic_it/main.py
+++ practica2/generic_it/main.py
@@ -13,6 +13,7 @@ from gestor_aplicacion.tienda.precio_componente import PrecioComponente
 from gestor_aplicacion.tienda.bodega import Bodega
 from gestor_aplicacion.tienda.producto import Producto
 from gestor_aplicacion.tienda.caja_registradora import CajaRegistradora
+from ui_main.field_frame import FieldFrame
 from ctypes import resize
 from tkinter import *
 import sys
@@ -144,69 +145,6 @@ def cobrar():
                 
 #------------------------------------------------------------------------------------------------------
 
-#Creación de la clase field frame, de la cual surgirán todos los formularios de la aplicación
-class FieldFrame(Frame):
-    def __init__(self, master, tituloCriterios, criterios, tituloValores, valores, habilitado):
-        self._tituloCriterios = tituloCriterios
-        self._criterios = criterios
-        self._tituloValores = tituloValores
-        self._valores = valores
-        self._habilitado = habilitado
-        self._entries = list()
-        super().__init__(master)
-        self.actualizacion()
-
-#Funcion que permite crear el grid del FieldFrame y actualizarlo con nuevos datos.
-    def actualizacion(self):
-        Label(self, text=self._tituloCriterios).grid(padx = 80, column = 0, row = 0)
-        Label(self, text=self._tituloValores).grid(padx = 80, column = 1, row = 0)
-        for i in range(1, len(self._valores)+1):
-            Label(self, text=self._criterios[i-1]).grid(padx = 80, pady=2, column=0, row=i)
-            if self._criterios[i-1] in self._habilitado:
-                texto = StringVar(value=self._valores[i-1])
-                entrada = Entry(self, width = 40, textvariable=texto, state=DISABLED, justify="center")
-            else:
-                texto = StringVar(value=self._valores[i-1])
-                entrada = Entry(self, width = 40, textvariable=texto, justify="center")
-        
-            entrada.grid(pady =2, column=1, row=i)
-            self._entries.append(entrada)
-
-        
-    #Funcion auxiliar del boton aceptar
-    def aceptarCheck(self):
-        #***ERIK***  EXCEPTION SI FALTAN CAMPOS POR LLENAR Y DECIR CUALES SON
-        for i in range(len(self._entries)): #_entries es la lista con las entradas.
-            self._valores[i] = self._entries[i].get()
-
-    
-    #Funcion auxiliar del boton borrar
-    def borrarEntry(self):
-        for entrada in self._entries:
-            entrada.delete(0, "end")
-        
-    def getValue(self, criterio):
-        criterios_dict = dict(zip(self._criterios, self._valores))
-        return criterios_dict[criterio]
-    
-    def getCriterios(self): 
-        return self._criterios
-        
-    def getValores(self):
-        return self._valores
-
-    def setValores(self, valores):
-        self._valores = valores
-    
-    def setEntries(self, entries):
-        self._entries = entries
-
-
-    def crearBotones(self, comando1):
-        aceptar = Button(self, text="Aceptar",command=comando1).grid(pady = 50, column = 0, row = len(self._criterios)+1)
-        borrar = Button(self, text="Borrar",command=self.borrarEntry).grid(pady = 50, column = 1, row = len(self._criterios)+1)
-        
-#Fin clase FieldFrame------------------------------------------------------------
 
 framesAMatar = []
 
