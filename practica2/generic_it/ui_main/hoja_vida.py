3fe643f80be45b96214cdd502b721b14eea5313c
Erik Gonzalez <erikhuboy@gmail.com>
2022-01-29 15:31:50 -0500

    actualiza para el proximo

--- practica2/generic_it/ui_main/hoja_vida.py
+++ practica2/generic_it/ui_main/hoja_vida.py
@@ -4,13 +4,24 @@ class HojaVida(Frame):
     def __init__(self, window):
         super().__init__(window)
         self.text = Text(self)
+        self.next_hv = 0
+
         self.photo = None
-        self.cargar_hv(1)
-        self.cargar_hv_imagen(1)
+        self.cargar_hv(0)
+        self.cargar_hv_imagen(0, 0)
 
+    def proximo(self, _):
+        if self.next_hv < 4:
+            self.next_hv = self.next_hv + 1
+        else:
+            self.next_hv = 0
 
-    def cargar_hv_imagen(self, numero):
-        self.photo = PhotoImage(file="./assets/goku.png")
+        self.cargar_hv(self.next_hv)
+        self.cargar_hv_imagen(self.next_hv, 0)
+
+    def cargar_hv_imagen(self, hv_num, numero):
+        path = './assets/hv_{0}_{1}.png'.format(hv_num, numero)
+        self.photo = PhotoImage(file=path)
         l = Label(self, image=self.photo)
         l.pack()
 
