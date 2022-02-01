64427e5193cd373083d56243b597019e0ad02383
Erik Gonzalez <erikhuboy@gmail.com>
2022-01-31 20:11:47 -0500

    excepciones

---
+++ practica2/generic_it/ui_main/numeric_exception.py
@@ -0,0 +1,12 @@
+'''
+@File    :   numeric_exception.py
+@Time    :   2022/01/31
+@Author  :   Erik Gonzalez
+@Version :   1.0
+@Desc    :   Error cuando se ingresan letras en vez de numeros
+'''
+
+from .field_exception import  FieldException
+
+class NumericException(FieldException):
+    pass
